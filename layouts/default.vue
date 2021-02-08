c0541d87021d673b5e2a259a8fe26ef8d0cfe72b
Vriyas Hartama <vriyashartama@gmail.com>
2021-02-08 12:44:14 +0700

    Unhide logo

--- layouts/default.vue
+++ layouts/default.vue
@@ -15,15 +15,11 @@ export default {
     this.$nextTick(function () {
       window.addEventListener('scroll', function () {
         const navbar = document.getElementById('nav')
-        const logo = document.getElementById('logo-image')
         const navClasses = navbar.classList
-        const logoClasses = logo.classList
         if (document.documentElement.scrollTop > 50) {
           navClasses.add('nav-shrink')
-          logoClasses.remove('image-hidden')
         } else {
           navClasses.remove('nav-shrink')
-          logoClasses.add('image-hidden')
         }
       })
     })
